82d8777632de6db4c38fea958c7857b6a28465ba
andrehebra <andrehebra@gmail.com>
2023-11-29 14:56:30 -0500

    rw_memory completed

--- project.c
+++ project.c
@@ -14,10 +14,12 @@ int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 {
     //word alignment --> if aligned get instruction and return 0
     if (PC % 4 == 0) {
-        //get the current instruction
+        //get the current instruction + shift
         unsigned shift = PC >> 2;
+        //save instruction
         *instruction = Mem[shift];
         
+        //return 0 because aligned and save
         return 0;
     }
 
@@ -46,6 +48,7 @@ int instruction_decode(unsigned op,struct_controls *controls)
 /* 5 Points */
 void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
 {
+    //save reg value into data1/2
     *data1 = Reg[r1];
     *data2 = Reg[r2];
 }
@@ -69,7 +72,23 @@ int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigne
 /* 10 Points */
 int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsigned *memdata,unsigned *Mem)
 {
+    if (MemWrite == 1) {
+        if (ALUresult % 4 == 0) {
+            Mem[ALUresult >> 2] = data2;
+        } else {
+            return 1;
+        }
+    }
 
+    if (MemRead == 1) {
+        if (ALUresult % 4 == 0) {
+            *memdata = Mem[ALUresult >> 2];    
+        } else {
+            return 1;
+        }
+    }
+
+    return 0;
 }
 
 
